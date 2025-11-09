31a5af70d17d19c7bb4a53a9890ac2314c014ab0
michaelpig0912 <michaelpig0912@gmail.com>
2025-11-09 09:38:50 +0000

    Site updated: 2025-11-09 09:38:35

---
+++ sideProject/MPsci/persistenceVisionPWA_beta/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'trajectory-recorder-beta-v2';
+const urlsToCache = [
+  '../persistenceVision_beta.html',
+  'manifest.json',
+  '192x192.png',
+  '512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // 检查请求的协议
+          if (event.request.url.startsWith('chrome-extension')) {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
